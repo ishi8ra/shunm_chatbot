024d6cc728bce0af124386d0e1720d1d6609e54a
ishihara-plasticity <fishihara41@gmail.com>
2023-10-15 20:40:13 +0900

    cssを修正

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ const io = socketIo(server);
 
 const PORT = 3000;
 
+// 静的ファイルを提供するディレクトリを設定
 app.use(express.static('/app/src'));
 
 app.get("/", (req: Request, res: Response) => {
@@ -22,8 +23,11 @@ server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
+// ユーザーが接続したときの処理
 io.on("connection", (socket: Socket) => {
   console.log("user connected");
+
+  // メッセージを受信したときの処理
   socket.on("sendMessage", (message) => {
     console.log("Message has been sent: ", message);
 
@@ -33,4 +37,22 @@ io.on("connection", (socket: Socket) => {
       io.emit("receiveMessage", message);
     }
   });
+
+  // ボットからのメッセージを受信したときの処理（フロントエンドで処理する場合）
+  socket.on("receiveMessage", (message) => {
+    // ここにフロントエンドでの処理を書く
+  });
 });
+
+// メッセージをDOMに追加する関数
+const addMessageList = (message: string, className: string) => {
+  const ul = document.getElementById("messageList");
+  if (ul) {
+    const li = document.createElement("li");
+    li.className = className;
+    const text = document.createTextNode(message);
+    li.appendChild(text);
+    ul.appendChild(li);
+    ul.scrollTop = ul.scrollHeight;
+  }
+};
